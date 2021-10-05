dea2a06b626ecfc2d8c44feeffce2f1b37b6524f
Maxwelljs <dragonmillo@gmail.com>
2021-10-04 19:42:45 -0500

    pues si

--- Consulta.sql
+++
@@ -1,4 +0,0 @@
-select unic_department.department_name, COUNT(unic_employe.id) from unic_department
-INNER JOIN unic_employe WHERE unic_employe.department_id = unic_department.id
-group by unic_department.department_name HAVING COUNT(unic_employe.id)>=2
-order by department_name ASC ;
